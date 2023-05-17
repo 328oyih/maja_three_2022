8bb73fd5b4d3c07030cf5d4bb7a1468ca655601e
328oyih <ellgyl@edu.umea.se>
2023-05-17 09:10:41 +0200

    did stuff

--- third-server/src/routes/(unauthenticated)/login/+page.server.ts
+++ third-server/src/routes/(unauthenticated)/login/+page.server.ts
@@ -9,18 +9,25 @@ export const actions: Actions = {
 		// TODO: Implement login
 		// Check if password and username
 		// exists and is correct
+		const client = await database.connect()
+		const db = client.db("test")
+		const collection = db.collection("users")
+
+		const USER = await collection.findOne({username: form.get("username")})
+		console.log(USER)
 
 		if (form.get('username') == "william") {
-			return invalid(400, { message: "username invalid" })
+			return invalid(400, {incorrect:true})
 		}
-
-		cookies.set('userid', 'secret', {
-			path: '/',
-			httpOnly: true, // optional for now
-			sameSite: 'strict',// optional for now
-			secure: process.env.NODE_ENV === 'production',// optional for now
-			maxAge: 120 //
-		})
+		if(true) {
+			cookies.set('userid', 'secret', {
+				path: '/',
+				httpOnly: true, // optional for now
+				sameSite: 'strict',// optional for now
+				secure: process.env.NODE_ENV === 'production',// optional for now
+				maxAge: 120 //
+			})
+		}
 
 		throw redirect(302, '/')
 
